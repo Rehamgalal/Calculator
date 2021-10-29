970f44864413684e7abbbac03c7dd09d2b659b0f
mac <rehamgalal1@outlook.com>
2021-10-29 16:12:09 +0200

    "setting up project"

---
+++ app/src/main/java/com/scan/calculator/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.scan.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.scan.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
